9cc6b10a3bd37cf0aff3f3a88405dac7e84c6bb7
Michael Kluge <vollseil@mailbox.org>
2023-01-28 19:31:02 +0100

    something comes back

--- index.ts
+++ index.ts
@@ -13,16 +13,28 @@ async function main() {
 SCHEDULED: <` +
         datum +
         ">";
-        const currentBlock = await logseq.Editor.getCurrentBlock();
-        if( currentBlock ) {
-          //console.log(currentBlock);
-          const newContent = "TODO " + currentBlock.content + dueDate;
-          await logseq.Editor.updateBlock( currentBlock.uuid, newContent);
-          //await logseq.Editor.exitEditingMode();
-        }
+      const currentBlock = await logseq.Editor.getCurrentBlock();
+      if (currentBlock) {
+        //console.log(currentBlock);
+        const newContent = "TODO " + currentBlock.content + dueDate;
+        await logseq.Editor.updateBlock(currentBlock.uuid, newContent);
+        //await logseq.Editor.exitEditingMode();
+      }
     });
   }
 }
 
-console.log("startup logseq easy schedule")
+import * as lcfg from "./config.json";
+import axios from "axios";
+console.log("loaded config with URL " + lcfg.API_URL);
+
+const data = await axios.get(lcfg.API_URL + "/projects", {
+  auth: {
+    username: "apikey",
+    password: lcfg.API_KEY,
+  },
+});
+console.log(data);
+
+console.log("startup logseq easy schedule");
 logseq.ready(main).catch(console.error);
