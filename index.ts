c5c6ff99bf17c005045b9d455dc952c5de242d8f
Michael Kluge <michael.kluge@tu-dresden.de>
2023-08-10 20:42:37 +0200

    wip: to be debugged

--- index.ts
+++ index.ts
@@ -5,131 +5,92 @@ import "@logseq/libs";
 import { settingsUI } from "./settings"
 import {
   Configuration,
-  ProjectsApi,
   WorkPackagesApi,
-  WorkPackagesApiListWorkPackagesRequest,
   UsersApi,
+  WorkPackagesApiListWorkPackagesRequest,
+  WorkPackageModel,
 } from "./openproject_api";
 
+var myWorkPackages : WorkPackageModel[]
+var usersApi : UsersApi = new UsersApi();
+var workPackagesApi : WorkPackagesApi = new WorkPackagesApi();
+var myself : string = ""
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>String Filter Popup</title>
-  <style>
-    /* Styles for the popup */
-    .popup-container {
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.5);
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      z-index: 1000;
-    }
-    .popup {
-      background-color: white;
-      padding: 20px;
-      border-radius: 5px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-    }
-  </style>
-</head>
-<body>
-  <!-- Button to open the popup -->
-  <button id="openButton">Open Popup</button>
-
-  <!-- Popup container (hidden by default) -->
-  <div class="popup-container" id="popupContainer" style="display: none;">
-    <div class="popup">
-      <input type="text" id="filterInput" placeholder="Filter...">
-      <div id="listContainer"></div>
-    </div>
-  </div>
-
-  <script>
-    const openButton = document.getElementById('openButton');
-    const popupContainer = document.getElementById('popupContainer');
-    const inputField = document.getElementById('filterInput') as HTMLInputElement;
-    const listContainer = document.getElementById('listContainer');
-
-    const itemList = ['Apple', 'Banana', 'Cherry', 'Grapes', 'Lemon', 'Orange', 'Peach', 'Strawberry'];
-
-    function updateFilteredList() {
+function getUIFunctions() {
+  return {    
+    updateFilteredList() {
+      const inputField = document.getElementById('filterInput') as HTMLInputElement;
+      const listContainer = document.getElementById('listContainer');
       const filterText = inputField.value.toLowerCase();
-      listContainer.innerHTML = '';
-
-      const filteredItems = itemList.filter(item => item.toLowerCase().includes(filterText));
-
-      filteredItems.forEach(item => {
-        const listItem = document.createElement('div');
-        listItem.textContent = item;
-        listContainer.appendChild(listItem);
-      });
-    }
-
-    inputField.addEventListener('input', updateFilteredList);
-
-    // Open the popup when the button is clicked
-    openButton.addEventListener('click', () => {
-      popupContainer.style.display = 'flex';
-      updateFilteredList();
-    });
-
-    // Close the popup when the ESC key is pressed
-    window.addEventListener('keydown', event => {
-      if (event.key === 'Escape') {
-        popupContainer.style.display = 'none';
+      if (listContainer) {
+        listContainer.innerHTML = ''; 
+        const filteredItems = myWorkPackages.filter(
+          item => item.subject.toLowerCase().includes(filterText)
+        );
+        filteredItems.forEach(item => {
+          const listItem = document.createElement('div');
+          listItem.textContent = item.subject;
+          listContainer.appendChild(listItem);
+        });
       }
-    });
-  </script>
-</body>
-</html>
-
-
-// Get references to the input field and the list container in your HTML
-const inputField = document.getElementById('filterInput') as HTMLInputElement;
-const listContainer = document.getElementById('listContainer');
-
-// Sample list of strings
-const itemList: string[] = ['Apple', 'Banana', 'Cherry', 'Grapes', 'Lemon', 'Orange', 'Peach', 'Strawberry'];
-
-// Function to update the displayed list based on the filter
-function updateFilteredList() {
-  const filterText = inputField.value.toLowerCase();
-
-  // Clear the existing list
-  listContainer.innerHTML = '';
-
-  // Filter the items based on the filter text
-  const filteredItems = itemList.filter(item => item.toLowerCase().includes(filterText));
-
-  // Display the filtered items
-  filteredItems.forEach(item => {
-    const listItem = document.createElement('div');
-    listItem.textContent = item;
-    listContainer.appendChild(listItem);
-  });
+    },
+    setupDialog() {
+      // Attach an event listener to the input field to trigger filtering
+      const inputField = document.getElementById('filterInput') as HTMLInputElement;
+      inputField.addEventListener('input', updateFilteredList);
+      // Initial rendering of the list
+      updateFilteredList();
+    }
+  }
 }
 
-// Attach an event listener to the input field to trigger filtering
-inputField.addEventListener('input', updateFilteredList);
+async function updateWorkPackages() {
+  const filterMyself = [{ assignee: { operator: "**", values: [myself] } }];
+  const filter = {
+    filters: JSON.stringify(filterMyself)
+  };
+  // FIXME: would like to call listWorkPackages with the filter
+  //        but it throws a server error, probably incorrect usage
+  //        of this feature
+  const workPackages = await workPackagesApi.listWorkPackages();
+  myWorkPackages = workPackages.data._embedded.elements.filter( wp => wp._links.assignee?.title == myself);
+}
 
-// Initial rendering of the list
-updateFilteredList();
+function website(x: number, y: number, text: string, tasks: string[] = []) {
+  return {
+    key: 'opeproject-task-selection',
+    close: 'outside',
+    template: `
+    <div padding: 10px; overflow: auto;>
+    <h3>${text}</h3>
+    <input type="text" id="filterInput" placeholder="Filter...">
+    <div id="listContainer"></div>
+    </div>
+  `,
+    style: {
+      left: x + 'px',
+      top: y + 'px',
+      width: '300px',
+      backgroundColor: 'var(--ls-primary-background-color)',
+      color: 'var(--ls-primary-text-color)',
+      boxShadow: '1px 2px 5px var(--ls-secondary-background-color)',
+    },
+    attrs: {
+      title: 'Select OpenProject Task ',
+    },
+  }
+}
 
+async function showUI( x: number, y: number) {
+  logseq.provideUI(website(x, y, 'Loading...'));
+  await updateWorkPackages();  
+  logseq.provideUI(website(x, y, 'Packages', myWorkPackages.map(wp => wp.subject)));
+}
 
 async function main() {
 
   var openProjectToken = "";
   var openProjectURL = "";
-  var usersApi : UsersApi = new UsersApi();
-  var workPackagesApi : WorkPackagesApi = new WorkPackagesApi();
   
   const loadSettings = () => {
     if (logseq.settings ) {
@@ -159,17 +120,15 @@ async function main() {
     console.log(me);
     return;
   }
-  const myself: String = me.data.name;
-  const filterMyself = [{ assignee: { operator: "**", values: [myself] } }];
-  const filter = {
-    filters: JSON.stringify(filterMyself)
-  };
-  // FIXME: would like to call listWorkPackages with the filter
-  //        but it throws a server error, probably incorrect usage
-  //        of this feature
-  const workPackages = await workPackagesApi.listWorkPackages();
-  const myWorkPackages = workPackages.data._embedded.elements.filter( wp => wp._links.assignee?.title == myself);
-  console.log(myWorkPackages);
+  myself = me.data.name;
+
+  logseq.provideModel(getUIFunctions());
+  logseq.Editor.registerSlashCommand("openproject", async () => {
+    const cursorat = await logseq.Editor.getEditingCursorPosition();
+    const x = cursorat ? cursorat.rect.x : 300;
+    const y = cursorat ? cursorat.rect.y : 300;
+    showUI( x, y);
+  });
 }
 
 logseq.ready(main).catch(console.error);
