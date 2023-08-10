0e5b6c41f8d4dc08d55ad9c4be908bce7172e320
Michael Kluge <michael.kluge@tu-dresden.de>
2023-08-10 10:55:56 +0200

    copy paste useful stuff

--- index.ts
+++ index.ts
@@ -12,6 +12,118 @@ import {
 } from "./openproject_api";
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>String Filter Popup</title>
+  <style>
+    /* Styles for the popup */
+    .popup-container {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.5);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      z-index: 1000;
+    }
+    .popup {
+      background-color: white;
+      padding: 20px;
+      border-radius: 5px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    }
+  </style>
+</head>
+<body>
+  <!-- Button to open the popup -->
+  <button id="openButton">Open Popup</button>
+
+  <!-- Popup container (hidden by default) -->
+  <div class="popup-container" id="popupContainer" style="display: none;">
+    <div class="popup">
+      <input type="text" id="filterInput" placeholder="Filter...">
+      <div id="listContainer"></div>
+    </div>
+  </div>
+
+  <script>
+    const openButton = document.getElementById('openButton');
+    const popupContainer = document.getElementById('popupContainer');
+    const inputField = document.getElementById('filterInput') as HTMLInputElement;
+    const listContainer = document.getElementById('listContainer');
+
+    const itemList = ['Apple', 'Banana', 'Cherry', 'Grapes', 'Lemon', 'Orange', 'Peach', 'Strawberry'];
+
+    function updateFilteredList() {
+      const filterText = inputField.value.toLowerCase();
+      listContainer.innerHTML = '';
+
+      const filteredItems = itemList.filter(item => item.toLowerCase().includes(filterText));
+
+      filteredItems.forEach(item => {
+        const listItem = document.createElement('div');
+        listItem.textContent = item;
+        listContainer.appendChild(listItem);
+      });
+    }
+
+    inputField.addEventListener('input', updateFilteredList);
+
+    // Open the popup when the button is clicked
+    openButton.addEventListener('click', () => {
+      popupContainer.style.display = 'flex';
+      updateFilteredList();
+    });
+
+    // Close the popup when the ESC key is pressed
+    window.addEventListener('keydown', event => {
+      if (event.key === 'Escape') {
+        popupContainer.style.display = 'none';
+      }
+    });
+  </script>
+</body>
+</html>
+
+
+// Get references to the input field and the list container in your HTML
+const inputField = document.getElementById('filterInput') as HTMLInputElement;
+const listContainer = document.getElementById('listContainer');
+
+// Sample list of strings
+const itemList: string[] = ['Apple', 'Banana', 'Cherry', 'Grapes', 'Lemon', 'Orange', 'Peach', 'Strawberry'];
+
+// Function to update the displayed list based on the filter
+function updateFilteredList() {
+  const filterText = inputField.value.toLowerCase();
+
+  // Clear the existing list
+  listContainer.innerHTML = '';
+
+  // Filter the items based on the filter text
+  const filteredItems = itemList.filter(item => item.toLowerCase().includes(filterText));
+
+  // Display the filtered items
+  filteredItems.forEach(item => {
+    const listItem = document.createElement('div');
+    listItem.textContent = item;
+    listContainer.appendChild(listItem);
+  });
+}
+
+// Attach an event listener to the input field to trigger filtering
+inputField.addEventListener('input', updateFilteredList);
+
+// Initial rendering of the list
+updateFilteredList();
+
+
 async function main() {
 
   var openProjectToken = "";
@@ -56,14 +168,7 @@ async function main() {
   //        but it throws a server error, probably incorrect usage
   //        of this feature
   const workPackages = await workPackagesApi.listWorkPackages();
-  const myWorkPackages = workPackages.data._embedded.elements.filter(function (
-    wp
-  ) {
-    if (wp._links.assignee?.title == myself) {
-      return true;
-    }
-    return false;
-  });
+  const myWorkPackages = workPackages.data._embedded.elements.filter( wp => wp._links.assignee?.title == myself);
   console.log(myWorkPackages);
 }
 
